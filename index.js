8d5fcafa48de0332148717704010c7fa98cb3173
yuki-yamaguchi-neits <124742960+yuki-yamaguchi-neits@users.noreply.github.com>
2025-04-22 10:52:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import fs from 'fs';
 import csv from 'csv-parser';
 import OpenAI from 'openai';
